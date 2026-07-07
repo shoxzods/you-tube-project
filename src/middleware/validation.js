07a176cf598e05827f7e0276378280df2efd6c26
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-07-07 19:53:01 +0500

    feat: added validation middlware ot auth router

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,8 +1,15 @@
+import { BadRequest } from "../utils/errors.js";
 import schema from "../utils/validation.js";
 
 function validation( req , res , next ) {
    const body = schema.validate(req.body);
-   console.log(body);
+   if ( body.error ) {
+      return next( new BadRequest(400 , body.error.message))
+   } else if (!body.value) {
+      return next(new BadRequest(400 , 'body is required'))
+   }
+
+   return next();
 }
 
 export default validation;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import { configDotenv } from "dotenv";
+import { join } from "path";
+import fs from "fs";
+
 configDotenv();
 import mainRouter from "./routers/main.router.js";
 
@@ -9,7 +12,15 @@ app.use(express.json());
 app.use(mainRouter);
 
 app.use(( err , req , res , next ) => {
-    console.log('error')
+    if ( err.status !== 500 ) {
+        fs.appendFileSync(join(process.cwd() , 'src' , 'logger' , 'error.txt') , `\n${new Date()}___error-name:${err.name}__error-message:${err.message}`)
+        return res.status(err.status).json({
+            success:false,
+            message:err.message
+        })
+    } else {
+        console.log('internal server error')
+    }
 })
 app.listen(process.env.PORT , () => {
     console.log('server is running')
