11911cb0f72daa7bcabcc7849b834e9936540f8f
HTML-CSS-Dev <ranaldu456@gmail.com>
2026-07-07 18:10:40 +0500

    feat:add .env ( PORT )

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from "express";
+import { configDotenv } from "dotenv";
+configDotenv();
+
 import mainRouter from "./routers/main.router.js";
 
 const app = express();
 
 app.use(mainRouter);
 
-app.listen(3000 , () => {
+app.listen(process.env.PORT , () => {
     console.log('server is running')
 });
